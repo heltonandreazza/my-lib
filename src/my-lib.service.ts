3fd7c4894e095a40f17518b46a66ef91989b9507
helton.andreazza <helton.andreazza@senior.com.br>
2017-05-26 21:30:54 -0300

    my commit

--- src/my-lib.service.ts
+++ src/my-lib.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/observable/forkJoin";
+import 'rxjs/add/observable/throw';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Injectable()
-export class MylibService {
+export class MyLibService {
 
-    constructor(http: Http) {
-        // ...
+    constructor(private http: Http) {
+    }
+
+    getGeoNames() {
+        return this.http.get('http://www.geonames.org/childrenJSON?geonameId=3469034')
+            .map(response => {
+                return response.json().geonames;
+            });
     }
 }
